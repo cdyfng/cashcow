3d6c56c5f0426c0151e6741aea32d9f9b7297dbf
cdyfng <xiaofeng_chen2007@163.com>
2018-12-24 05:49:03 +0000

    okex ws update

---
+++ exchange/okcoin-ws/okws.js
@@ -0,0 +1,99 @@
+const pako = require("pako");
+
+const WebSocket = require("ws");
+let cache = require("../../common/cache");
+
+// url
+const ws = new WebSocket("wss://real.okex.com:10441/websocket?compress=true");
+
+ws.on("open", function open() {
+  // ws.send('{"channel":"ok_sub_futureusd_btc_depth_quarter","event":"addChannel"}')
+  ws.send('{"channel":"ok_sub_spot_eth_btc_depth_5","event":"addChannel"}');
+  ws.send('{"channel":"ok_sub_spot_eos_btc_depth_5","event":"addChannel"}');
+  ws.send('{"channel":"ok_sub_spot_bch_btc_depth_5","event":"addChannel"}');
+  ws.send('{"channel":"ok_sub_spot_bsv_btc_depth_5","event":"addChannel"}');
+});
+
+ws.on("message", function incoming(data) {
+  if (data instanceof String) {
+    console.log("1", data);
+  } else {
+    try {
+      check_depth(JSON.parse(pako.inflateRaw(data, { to: "string" })));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
+
+let channel_symbol = new Map([
+  ["ok_sub_spot_eth_btc_depth_5", "ETH/BTC"],
+  ["ok_sub_spot_eos_btc_depth_5", "EOS/BTC"],
+  ["ok_sub_spot_bch_btc_depth_5", "BCH/BTC"],
+  ["ok_sub_spot_bsv_btc_depth_5", "BSV/BTC"]
+]);
+
+async function check_depth(d) {
+  //prices, symbol
+  try {
+    let channel = d[0].channel;
+    //console.log("channel: :", typeof d, channel, d);
+    for (var [key, symbol] of channel_symbol) {
+      //console.log("loop: ", key, symbol);
+      if (channel == key) {
+        //console.log("processing", key + " = " + symbol);
+        let data = d[0].data;
+        let spread = +data.asks[data.asks.length - 1][0] - +data.bids[0][0];
+
+        let depth = {
+          website_time: new Date(data.timestamp),
+          exchange: "okex",
+          symbol: symbol,
+          ask0: [
+            +data.asks[data.asks.length - 1][0],
+            +data.asks[data.asks.length - 1][1]
+          ],
+          bid0: [+data.bids[0][0], +data.bids[0][1]]
+        };
+        let asks = [];
+        data.asks.forEach(function(i) {
+          asks.push([+i[0], +i[1]]);
+        });
+        asks = asks.reverse();
+        let bids = [];
+        data.bids.forEach(function(i) {
+          bids.push([+i[0], +i[1]]);
+        });
+        depth.asks = asks;
+        depth.bids = bids;
+
+        console.log(
+          new Date(),
+          symbol,
+          "Bid: ",
+          bids[0][0],
+          "@",
+          bids[0][1],
+          "Ask:",
+          asks[0][0],
+          "@",
+          asks[0][1],
+          "Spread",
+          spread,
+          "rate",
+          asks[0][0] / bids[0][0]
+        );
+        //console.log("bids:", bids);
+        //console.log("asks:", asks);
+        cache.set("okex" + symbol, depth, function() {
+          //console.log('b.')
+          // cache.get('bitfinexBCH/BTC', function (err, data) {
+          //   console.log('data: ', data)
+          // })
+        });
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
